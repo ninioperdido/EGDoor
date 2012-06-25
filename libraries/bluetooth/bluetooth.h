65cbbcacd8d32c5fc5a9d05e5a6950010d01f2a7
Gerardo Puerta Cardelles <gerardo@gerardo-desktop.(none)>
2012-06-25 16:36:34 +0200

    First steps with the library ...

---
+++ libraries/bluetooth/bluetooth.h
@@ -0,0 +1,56 @@
+/*
+ *  Bluetooth.h - Library for controlling
+ *  the seeeduino bluetooth shield.
+ *  (cc) 2012 Gerardo Puerta <gerardo>at<gnuardo.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  
+ *  02110-1301  USA
+ *
+ */
+
+#include "Arduino.h"
+
+#ifndef Bluetooth_h
+#define Bluetooth_h
+
+class Bluetooth
+{
+
+ public:
+  Bluetooth(int rx, int tx);
+  void setWorkingMode(String mode);
+  void setBaudRate(int br);
+  void setDeviceName(String name);
+  void autoConnect(bool activate);
+  void permitConnect(bool activate);
+  void setPinCode(long pin);
+  void deletePinCode();
+
+  String readLocalAddress();
+  String inquiry();
+  String connectToDevice(String address);
+  String status();
+
+ private:
+  int rx;
+  int tx;
+  String pincode;
+  String mode;
+  bool autoconnect;
+  bool permitconnect;
+}
+
+#endif
+
