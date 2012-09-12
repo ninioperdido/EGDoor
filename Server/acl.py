1e1dc7202a229d765d3d409912fe7c89465a3716
Gerardo Puerta <gerardo@gnuardo.com>
2012-09-12 12:33:04 +0200

    Load and save acl from disk

--- Server/acl.py
+++ Server/acl.py
@@ -3,10 +3,10 @@
 # www.limscoder.com/2009/11/role-based-security-with-python.html
 
 from xml.dom.minidom import parse
- 
+import cPickle as pickle
+
 class AccessError(Exception):
     pass
-
 
 from datetime import datetime, time, date
 
@@ -29,11 +29,13 @@ class TimedAccess(object):
       if since == None:
         self._since = now
       else:
+        assert type(since) == type(date.today())
         self._since = since
 
       if to == None:
         self._to = max
       else:
+        assert type(to) == type(date.today())
         self._to = to
       
       print self
@@ -58,7 +60,6 @@ class TimedAccess(object):
         dow = self._weekdays[getattr(dt,'tm_wday')]
         now = datetime.now().time()
         for time, access in dow.iteritems():
-            print "Time %s, Access %s" % (time, access)
             if access and time <= now:
               haccess = True
             if not access and time >= now:
@@ -91,7 +92,7 @@ class Resource(object):
         if privilege in self._privileges \
            and privilege in self._timedaccess:
           return self._privileges[privilege] \
-                 and self._timedaccess[privile].has_acces()
+                 and self._timedaccess[privilege].has_access()
         if privilege in self._privileges:
             return self._privileges[privilege]
         return False
@@ -100,6 +101,8 @@ class Resource(object):
         rpr = self.name + ': '
         for privilege, access in self._privileges.iteritems():
             rpr += "%s:%s " % (privilege, access)
+        for privilege, timedaccess in self._timedaccess.iteritems():
+            rpr += "%s:%s " % (privilege, timedacess)
         return rpr
  
 class Role(object):
@@ -133,123 +136,69 @@ class Role(object):
             rpr += "\t%s\n" % parent
         rpr += "resources:\n"
         for resource in self._resources.values():
-            rpr += "\t%s\n" % resource.describe()
+            rpr += "\t%s\n" % resource
         return rpr
- 
+
+
 class Acl(object):
-    """Manages roles and resources.
-     
-    Singleton class.
-    """
- 
-    class __impl:
-        """Implementation of the singleton interface"""
-         
-        def __init__(self):
-            self._acl = {}
- 
-        def set_role(self, role):
-            self._acl[role.name] = role
-             
-        def check_access(self, role_name, resource, privilege):
-            """Check whether a role has access to a resource or not."""
- 
-            if not role_name in self._acl:
-                raise AccessError('Role does not exist.')
-            return self._acl[role_name].has_access(resource, privilege)
- 
-        def build_acl(self, file):
-            """Build acl from an XML file."""
- 
-            self._acl = {}
-            roles_to_create = {}
-            dom = parse(file)
-             
-            # Find roles to create
-            roles_nodes = dom.getElementsByTagName('roleSet')
-            for roles_node in roles_nodes:
-                role_nodes = roles_node.getElementsByTagName('role')
-                for role_node in role_nodes:
-                    name_nodes = role_node.getElementsByTagName('name')
-                    parent_nodes = role_node.getElementsByTagName('inheritFrom')
-                    role_name = name_nodes[0].childNodes[0].data
-                    roles_to_create[role_name] = []
- 
-                    # Find role parents
-                    for parent_node in parent_nodes:
-                        roles_to_create[role_name].append(parent_node.childNodes[0].data)
- 
-            # build inheritence chain
-            for role, parents in roles_to_create.iteritems():
-                self.set_role(self._create_role(role, roles_to_create))
- 
-            # assign permissions
-            permissions = dom.getElementsByTagName('permissions')
-            for permissions_node in permissions:
-                permission_nodes = permissions_node.getElementsByTagName('permission')
-                for permission_node in permission_nodes:
-                    resource_nodes = permission_node.getElementsByTagName('resource')
-                    role_nodes = permission_node.getElementsByTagName('role')
-                    privilege_nodes = permission_node.getElementsByTagName('privilege')
- 
-                    for resource_node in resource_nodes:
-                       resource = Resource()
-                       resource.name = resource_node.childNodes[0].data
-                       for privilege_node in privilege_nodes:
-                           resource.set_privilege(privilege_node.childNodes[0].data)
- 
-                       for role_node in role_nodes:
-                           try:
-                               role = self._acl[role_node.childNodes[0].data]
-                           except:
-                               raise AccessError('Role in permission is not defined.')
- 
-                           role.set_resource(resource)
- 
-        def _create_role(self, role_name, roles_to_create):
-            """Recursively create parent roles and then create child role."""
-             
-            if role_name in self._acl:
-                role = self._acl[role_name]
-            else:
-                role = Role()
-                role.name = role_name
-                 
-            for parent_name in roles_to_create[role_name]:
-                if parent_name in self._acl:
-                    parent = self._acl[parent_name]
-                else:
-                    parent = self._create_role(parent_name, roles_to_create)
-                    self.set_role(parent)
-                role.set_parent(parent)
-            return role
-         
-        def __str__(self):
-            rpr = ''
-            for role in self._acl.values():
-                rpr += '----------\n'
-                rpr += role.describe()
-            return rpr
- 
-    __instance = None
- 
+    """Manages roles and resources."""
     def __init__(self):
-        """ Create singleton instance """
+        self._acl = {}
+
+    def set_role(self, role):
+        self._acl[role.name] = role
          
-        # Check whether an instance already exists.
-        # If not, create it.
-        if Acl.__instance is None:
-            Acl.__instance = Acl.__impl()
- 
-        self.__dict__['_Acl__instance'] = Acl.__instance
- 
-    def __getattr__(self, attr):
-        """ Delegate get access to implementation """
- 
-        return getattr(self.__instance, attr)
- 
-    def __setattr__(self, attr, val):
-        """ Delegate set access to implementation """
- 
-        return setattr(self.__instance, attr, val)
+    def check_access(self, role_name, resource, privilege):
+        """Check whether a role has access to a resource or not."""
+
+        if not role_name in self._acl:
+            raise AccessError('Role does not exist.')
+        return self._acl[role_name].has_access(resource, privilege)
+
+
+    def _create_role(self, role_name, roles_to_create):
+        """Recursively create parent roles and then create child role."""
+         
+        if role_name in self._acl:
+            role = self._acl[role_name]
+        else:
+            role = Role()
+            role.name = role_name
+             
+        for parent_name in roles_to_create[role_name]:
+            if parent_name in self._acl:
+                parent = self._acl[parent_name]
+            else:
+                parent = self._create_role(parent_name, roles_to_create)
+                self.set_role(parent)
+            role.set_parent(parent)
+        return role
+     
+    def __str__(self):
+        rpr = ''
+        for role in self._acl.values():
+            rpr += '----------\n'
+            rpr += str(role)
+        return rpr
+
+    def Role(self, name):
+      self._acl[name] = Role(name)
+      return self._acl[name]
+
+    def Resource(self, name):
+      return Resource(name)
+
+class Config(object):
+  """ class to dump and load object configuration """
+  conf_path = "./."
+  conf_ext =  ".egd"
+
+  @staticmethod
+  def save(obj):
+    pickle.dump(obj, open(Config.conf_path+str(type(obj).__name__)+Config.conf_ext, "wb"))
+
+  @staticmethod
+  def load(tobj):
+    return pickle.load(open(Config.conf_path+tobj+Config.conf_ext, "rb"))
+
 
